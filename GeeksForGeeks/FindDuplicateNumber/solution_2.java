a460f12dd92e2ee044b9203f196c8fb79b816ad1
DecryptLife <benson.thomas2602@gmail.com>
2021-10-12 07:30:48 +0530

    Using Set

--- GeeksForGeeks/FindDuplicateNumber/solution_2.java
+++ GeeksForGeeks/FindDuplicateNumber/solution_2.java
@@ -3,5 +3,23 @@ package FindDuplicateNumber;
 import java.util.*;
 public class solution_2 {
 
-   
+    public static int findDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<Integer>();
+        
+        for(int num:nums)
+        {
+            if(seen.contains(num))
+                return num;
+            else
+                seen.add(num);
+        }
+        
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,1,1,1};
+
+        System.out.println(findDuplicate(arr));
+    }
 }
