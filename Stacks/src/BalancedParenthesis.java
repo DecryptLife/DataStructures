4c360bd78210a5187cf1d6b5839fdde60185d3ce
DecryptLife <benson.thomas2602@gmail.com>
2022-10-09 16:59:29 -0500

    Modified Balanced Parenthesis

--- Stacks/src/BalancedParenthesis.java
+++ Stacks/src/BalancedParenthesis.java
@@ -1,9 +1,20 @@
 import java.util.ArrayDeque;
+import java.util.Stack;
 
 public class BalancedParenthesis {
 
+    static void stackOp(ArrayDeque<Character> stack, char val)
+    {
+        if(stack.isEmpty())
+            stack.push(val);
+        else
+            if(val == stack.peek())
+                stack.pop();
+            else
+                stack.push(val);
+    }
     public static void main(String[] args) {
-        String s = "{}([()])";
+        String s = "{}[([])]";
         ArrayDeque<Character> stack = new ArrayDeque<>();
         for(int i = 0;i<s.length();i++)
         {
@@ -13,24 +24,23 @@ public class BalancedParenthesis {
                 {
                     case '}':
                         val =  '{';
+                        stackOp(stack,val);
                         break;
                     case ']':
                         val = '[';
+                        stackOp(stack,val);
                         break;
                     case ')':
                         val = '(';
+                        stackOp(stack,val);
                         break;
                     default:
                         throw new IllegalArgumentException("Unexpecred argument: "+val);
                 }
-
-            if(stack.isEmpty())
-                stack.push(val);
             else
-                if(val == stack.peek())
-                    stack.pop();
-                else
-                    stack.push(val);
+                stack.push(val);
+
+
         }
         if(stack.isEmpty())
             System.out.println("Balanced Parenthesis");
