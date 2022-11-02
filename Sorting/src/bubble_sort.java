fae93338146c7d54e19ba81e1f18ffaf4681218f
DecryptLife <benson.thomas2602@gmail.com>
2022-11-02 12:34:44 -0500

    Better optimized

--- Sorting/src/bubble_sort.java
+++ Sorting/src/bubble_sort.java
@@ -1,10 +1,11 @@
 public class bubble_sort {
     public static void main(String[] args) {
         int[] arr = {2,10,8,7};
-
+        boolean swapped;
         for(int i=0;i<arr.length-1;i++)
         {
             System.out.println("Pass 1 ");
+            swapped  =false;
             for(int j=0;j<arr.length-1-i;j++)
             {
 
@@ -13,19 +14,17 @@ public class bubble_sort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
-                for(int k=0;k<arr.length;k++)
-                {
+                for(int k=0;k<arr.length;k++) {
                     System.out.print(arr[k] + " ");
                 }
                 System.out.println();
             }
+            if(swapped == false)
+                break;
 
         }
 
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.print(arr[i] + " ");
-        }
     }
 }
